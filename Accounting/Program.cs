be09dd283969dc9b656cc438b10bc3975cfa8a3a
Grant Parkis <gparkis@gmail.com>
2018-10-13 11:01:32 -0400

    Welcome screen

--- Accounting/Program.cs
+++ Accounting/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Accounting
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
         }
     }
 }
